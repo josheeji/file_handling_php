e98dec9c612de3d87db17778d833b08f810b3a24
unknown <jjoshiganeshraj@gmail.com>
2022-06-05 18:38:00 +0545

    store is deleted

--- addStudent.php
+++ addStudent.php
@@ -126,10 +126,12 @@
     <label for="class" class="form-label">Class</label>
     <input type="class" class="form-control" id="class" aria-describedby="class">
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  
+  <div class="mb-3">
+    <label for="gender" class="form-label"> Gender</label>
+    <input type="gender" class="form-control" id="gender" aria-describedby="gender">
   </div>
+  
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
